1b81ebdac54b3d97a57b870f7332836a5fbeb2d2
shinjjune <sjh8236@naver.com>
2019-11-11 15:49:36 +0900

    eslint-config-airbnb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './static/css/clean-blog.min.css';
 
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
