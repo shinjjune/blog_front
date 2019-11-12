dab72024831d5a7669fe26e2e1d88e52dd03c261
shinjjune <sjh8236@naver.com>
2019-11-12 09:48:52 +0900

    morning update191112

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import Nav from './Nav';
-
+import React from "react";
+import Nav from "./Nav";
+import Header from "./Header";
+import Home from "./Home";
 export default function App() {
   return (
     <>
       <Nav />
+      <Header />
+      <Home />
     </>
   );
 }
